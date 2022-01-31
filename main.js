fcd3e5de74534efadf10ce59bb0536590cff334a
shopnilashik <57426443+shopnilashik@users.noreply.github.com>
2022-01-31 15:43:13 +0600

    Scroll Effect

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ btn.addEventListener('click', () => {
 	menu.classList.toggle('hidden');
 	console.log("hidden done")
 });
+
+window.addEventListener('scroll', () => {
+	let content = document.querySelector('.work');
+	let contentPosition = content.getBoundingClientRect().top;
+	let secontPosition = window.innerHeight / 3;
+	if (contentPosition < secontPosition) {
+		content.classList.add('active');
+	}
+	else {
+		content.classList.remove('active');
+	}
+})
